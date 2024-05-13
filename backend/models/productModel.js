1ec10c3677255cb5c535d06b1d2be87f3bff8b2b
Alumist <codealumist@gmail.com>
2024-05-13 14:07:11 -0700

    database

---
+++ backend/models/productModel.js
@@ -0,0 +1,89 @@
+import mongoose from 'mongoose';
+import { timestamp } from 'rxjs';
+
+// Create a schema for the product
+// A schema is a blueprint for a MongoDB collection
+// A schema defines the structure of the documents that you can store in a MongoDB collection
+// The schema tells the database what the data looks like, what fields it has, what type each field is, and how those fields should be validated
+// The schema is used to enforce data integrity in the database
+
+const reviewSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
